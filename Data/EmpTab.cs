91a4724c06f00d28f80f061732b0d3d24482211b
Prajwal <105980527+Prajwalbs123@users.noreply.github.com>
2024-08-22 18:15:10 +0530

    Add files via upload

---
+++ Data/EmpTab.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using MeetingsAPI.DTO;
+
+namespace MeetingsAPI.Data
+{
+
+    public partial class EmpTab
+    {
+        public int EmpId { get; set; }
+
+        public string EmpName { get; set; } = null!;
+
+        public string EmpRole { get; set; } = null!;
+
+        public string EmpEmail { get; set; } = null!;
+
+        public string EmpPassword { get; set; } = null!;
+
+        public bool Available { get; set; }
+
+        public virtual ICollection<SlotTab> SlotTabs { get; set; } = new List<SlotTab>();
+
+        public void EmpMap(EmpDto emp)
+        {
+            this.EmpId = emp.EmpId;
+            this.EmpName = emp.EmpName;
+            this.EmpRole = emp.EmpRole;
+            this.EmpEmail = emp.EmpEmail;
+            this.EmpPassword = emp.EmpPassword;
+            this.Available = emp.Available;
+			foreach (var slot in emp.SlotTabs)
+			{
+				SlotTab slotTab = new SlotTab();
+				slotTab.SlotMap(slot);
+				this.SlotTabs.Add(slotTab);
+			}
+		}
+    }
+}
---
+++ Data/SlotTab.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using MeetingsAPI.DTO;
+
+namespace MeetingsAPI.Data
+{
+
+    public partial class SlotTab : IComparable<SlotTab>
+    {
+        public int SlotId { get; set; }
+
+        public int? RoomId { get; set; }
+
+        public int? EmpId { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public TimeOnly STime { get; set; }
+
+        public TimeOnly ETime { get; set; }
+
+        public bool Active { get; set; }
+
+        public virtual EmpTab? Emp { get; set; }
+
+        public virtual RoomTab? Room { get; set; }
+
+        public int CompareTo(SlotTab? other)
+        {
+            if (this.Date != other.Date) return this.Date.CompareTo(other.Date);
+
+            return this.STime.CompareTo(other.STime);
+        }
+
+        public void SlotMap(SlotDto slot)
+        {
+            this.SlotId = slot.SlotId;
+            this.RoomId = slot.RoomId;
+            this.EmpId = slot.EmpId;
+            this.Date = slot.Date;
+            this.Active = slot.Active;
+            this.STime = slot.STime;
+            this.ETime = slot.ETime;
+
+			EmpTab? emp = new EmpTab();
+			emp.EmpMap(slot.Emp);
+			this.Emp = emp;
+
+			RoomTab? room = new RoomTab();
+			room.RoomMap(slot.Room);
+			this.Room = room;
+
+		}
+    }
+
+}
