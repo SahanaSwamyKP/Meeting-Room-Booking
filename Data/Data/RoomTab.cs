ffe2711a53da87fc3a05c0c7c9f15c84afa1e451
Prajwal <105980527+Prajwalbs123@users.noreply.github.com>
2024-08-26 16:33:37 +0530

    Add files via upload

---
+++ Data/Data/RoomTab.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using MeetingsAPI.DTO;
+
+namespace MeetingsAPI.Data
+{
+
+    public partial class RoomTab
+    {
+        public int RoomId { get; set; }
+
+        public string RoomName { get; set; } = null!;
+
+        public int Capacity { get; set; }
+
+        public bool Available { get; set; }
+
+        public virtual ICollection<SlotTab> SlotTabs { get; set; } = new List<SlotTab>();
+
+		public void RoomMap(RoomDto room)
+		{
+			this.RoomId = room.RoomId;
+			this.RoomName = room.RoomName;
+			this.Capacity = room.Capacity;
+			this.Available = room.Available;
+			foreach (var slot in room.SlotTabs)
+			{
+				SlotTab slotTab = new SlotTab();
+				slotTab.SlotMap(slot);
+				this.SlotTabs.Add(slotTab);
+			}
+		}
+	}
+}
