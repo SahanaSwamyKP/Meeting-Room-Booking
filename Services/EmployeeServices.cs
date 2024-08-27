4184f0d4918c501244d04c56f39bc3b9500354f6
Prajwal <105980527+Prajwalbs123@users.noreply.github.com>
2024-08-27 20:59:31 +0530

    Add files via upload

---
+++ Services/EmployeeServices.cs
@@ -0,0 +1,174 @@
+﻿using System.Text;
+using MeetingsAPI.Data;
+using MeetingsAPI.DTO;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace MeetingsAPI.Services
+{
+	public interface IEmployeeServices
+	{
+		void AddEmployee(EmpDto emp);
+		EmpDto GetEmpbyId(EmpDto emp);
+		EmpDto GetEmpbyEmail(EmpDto emp);
+		List<EmpDto> GetEmployees();
+		void UpdateEmployee(EmpDto emp);
+		void UpdatePassword(EmpDto emp);
+		EmpDto? GetEmailForChange(EmpDto emp);
+		void UpdateEmployeeById(int id, EmpDto emp);
+	}
+
+	public class EmployeeServices : IEmployeeServices
+	{
+		private readonly ProjectContext _projectContext;
+		public EmployeeServices(ProjectContext projectContext) { this.empDId = null; this.empDE = null; this._projectContext = projectContext; }
+
+		public List<EmpDto> GetEmployees()
+		{
+			var emplist = _projectContext.EmpTabs.ToList();
+			var empDtolist = new List<EmpDto>();
+			foreach (var emp in emplist)
+			{
+				EmpDto empDto = new EmpDto();
+				empDto.EmpMap(emp);
+				empDtolist.Add(empDto);
+			}
+			return empDtolist;
+		}
+
+		EmpTab? empDId;
+		public EmpDto GetEmpbyId(EmpDto emp)
+		{
+			this.empDId = _projectContext.EmpTabs.FirstOrDefault(x => x.EmpId == emp.EmpId);
+			if (empDId == null) throw new Exception("Employee Doesn't Exist");
+
+			var empDto = new EmpDto();
+			empDto.EmpMap(this.empDId);
+			return empDto;
+		}
+
+		//Required for Login
+		EmpTab? empDE;
+		public EmpDto? GetEmpbyEmail(EmpDto emp)
+		{
+			empDE = _projectContext.EmpTabs.FirstOrDefault(x => x.EmpEmail == emp.EmpEmail);
+			if (empDE == null) return null;
+			if (empDE.Available == false) return null;
+			byte[] salt = Encoding.UTF8.GetBytes(emp.EmpEmail);
+			string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: emp.EmpPassword!,
+				salt: salt,
+				prf: KeyDerivationPrf.HMACSHA512,
+				iterationCount: 100000,
+				numBytesRequested: 512 / 8)
+				);
+			emp.EmpPassword = hashed;
+			if (empDE.EmpPassword != emp.EmpPassword) throw new Exception("Incorrect password");
+			var empDto = new EmpDto();
+			empDto.EmpMap(empDE);
+			return empDto;
+		}
+
+		public EmpDto? GetEmailForChange(EmpDto emp)
+		{
+			empDE = _projectContext.EmpTabs.FirstOrDefault(x => x.EmpEmail == emp.EmpEmail);
+			if (empDE == null) return null;
+			var empDto = new EmpDto();
+			empDto.EmpMap(empDE);
+			return empDto;
+		}
+
+		public void AddEmployee(EmpDto emp)
+		{
+			if (emp == null) throw new Exception("Employee Cannot be NULL");
+			if (emp.EmpRole != "Admin" && emp.EmpRole != "Employee") throw new Exception("Employee Role Doesn't Exist");
+
+			var employee = GetEmailForChange(emp);
+			if (employee != null) throw new Exception("Employee With This Email Already Exist");
+
+
+			//hashing... 
+			byte[] salt = Encoding.UTF8.GetBytes(emp.EmpEmail);
+			string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: emp.EmpPassword!,
+				salt: salt,
+				prf: KeyDerivationPrf.HMACSHA512,
+				iterationCount: 100000,
+				numBytesRequested: 512 / 8)
+				);
+			//
+			emp.EmpPassword = hashed;
+
+			var empTab = new EmpTab();
+			empTab.EmpMap(emp);
+			_projectContext.EmpTabs.Add(empTab);
+			_projectContext.SaveChanges();
+		}
+
+		public void UpdatePassword(EmpDto emp)
+		{
+			var rec = GetEmailForChange(emp);
+			if (rec == null) throw new Exception("Employee Doesn't Exist to Update Password");
+			//hashing...
+			byte[] salt = Encoding.UTF8.GetBytes(emp.EmpEmail);
+			string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: emp.EmpPassword!,
+				salt: salt,
+				prf: KeyDerivationPrf.HMACSHA512,
+				iterationCount: 100000,
+				numBytesRequested: 512 / 8)
+				);
+			//
+			rec.EmpPassword = hashed;
+			if (empDE == null) throw new Exception("Employee Doesn't Exist To Update Password");
+
+			empDE.EmpMap(rec);
+			_projectContext.SaveChanges();
+		}
+
+		public void UpdateEmployee(EmpDto emp)
+		{
+			EmpDto employee;
+			try
+			{
+				employee = GetEmpbyId(emp);
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+
+			employee.EmpName = emp.EmpName;
+			employee.Available = emp.Available;
+			employee.EmpRole = emp.EmpRole;
+
+			employee.EmpEmail = emp.EmpEmail;
+			//hashing...
+			byte[] salt = Encoding.UTF8.GetBytes(emp.EmpEmail);
+			string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: emp.EmpPassword!,
+				salt: salt,
+				prf: KeyDerivationPrf.HMACSHA512,
+				iterationCount: 100000,
+				numBytesRequested: 512 / 8)
+				);
+			//
+			employee.EmpPassword = hashed;
+
+			if (empDId == null) throw new Exception("Employee Doesn't Exist To Update");
+
+			empDId.EmpMap(employee);
+			_projectContext.SaveChanges();
+		}
+
+		public void UpdateEmployeeById(int id, EmpDto emp)
+		{
+			var employee = _projectContext.EmpTabs.FirstOrDefault(e => e.EmpId == id) ?? throw new Exception();
+			employee.EmpName = emp.EmpName;
+			employee.EmpEmail = emp.EmpEmail;
+			employee.EmpRole = emp.EmpRole;
+			employee.Available = emp.Available;
+			employee.EmpMap(emp);
+			_projectContext.SaveChanges();
+		}
+	}
+}
---
+++ Services/SlotServices.cs
@@ -0,0 +1,73 @@
+﻿using MeetingsAPI.Data;
+using MeetingsAPI.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingsAPI.Services
+{
+	public class SlotServices
+	{
+		private readonly ProjectContext _projectContext;
+
+		public SlotServices(ProjectContext projectContext)
+		{
+			_projectContext = projectContext;
+		}
+		public List<SlotDto> GetSlotsByRoom(RoomDto room)
+		{
+			List<SlotDto> slotsdto = new List<SlotDto>();
+			var slots = _projectContext.SlotTabs.ToList();
+			foreach (var slot in slots)
+			{
+				if (slot.RoomId == room.RoomId)
+				{
+					SlotDto slotDto = new SlotDto();
+					slotDto.SlotMap(slot);
+					if (!slotsdto.Contains(slotDto)) slotsdto.Add(slotDto);
+				}
+			}
+			return slotsdto;
+		}
+
+		public List<SlotDto> GetSlots()
+		{
+			List<SlotDto> slotsDto = new List<SlotDto>();
+			var slots = _projectContext.SlotTabs.ToList();
+			foreach (var slot in slots)
+			{
+				SlotDto slotDto = new SlotDto();
+				slotDto.SlotMap(slot);
+				slotsDto.Add(slotDto);
+			}
+			slotsDto.Sort();
+			return slotsDto;
+		}
+
+		public void delete(SlotDto slotDto)
+		{
+			var slot = _projectContext.SlotTabs.FirstOrDefault(x => x.SlotId == slotDto.SlotId);
+			if (slot != null) slot.Active = false;
+			_projectContext.SaveChanges();
+		}
+
+		public void AddSlot(SlotDto slotDto)
+		{
+
+			if (slotDto == null) throw new Exception("Empty Slot Error");
+			var slot = _projectContext.SlotTabs.FirstOrDefault(x => x.Date == DateOnly.FromDateTime(slotDto.Date) && x.RoomId == slotDto.RoomId && (TimeOnly.FromDateTime(slotDto.STime) >= x.STime && TimeOnly.FromDateTime(slotDto.STime) <= x.ETime));
+			if (slot != null) throw new Exception("Slot in this room is already booked");
+
+			SlotTab new_slot = new();
+			new_slot.SlotMap(slotDto);
+			_projectContext.SlotTabs.Add(new_slot);
+			_projectContext.SaveChanges();
+		}
+
+		public void GetUnavailable(SlotDto slotDto)
+		{
+			if (slotDto.Date < DateTime.Now) throw new Exception("Past Date or Time Error");
+			if (slotDto.STime > slotDto.ETime) throw new Exception("Start time should be less than end time");
+			if (slotDto.ETime - slotDto.STime < TimeSpan.FromMinutes(30)) throw new Exception("Atleast 30 mins slot should be booked");
+		}
+
+	}
+}
