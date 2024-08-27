327d90efb6e3add82925e20b00e14cab0055ed28
Prajwal <105980527+Prajwalbs123@users.noreply.github.com>
2024-08-27 20:54:56 +0530

    Delete src directory

--- src/app/models/room-tab.ts
+++
@@ -1,9 +0,0 @@
-import { SlotTab } from "./slot-tab";
-
-export interface RoomTab {
-    roomId: number,
-    roomName: string,
-    capacity: number,
-    available: boolean,
-    slotTabs: SlotTab
-}
--- src/src/app/models/emp-tab.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmpTab } from './emp-tab';
-
-describe('EmpTab', () => {
-  it('should create an instance', () => {
-    expect(new EmpTab()).toBeTruthy();
-  });
-});
