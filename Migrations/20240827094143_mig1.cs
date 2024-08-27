46cd209f641b8356b2bd84a5c555b7ebf4147cb1
Prajwal <105980527+Prajwalbs123@users.noreply.github.com>
2024-08-27 20:58:58 +0530

    Add files via upload

---
+++ Migrations/20240827094143_mig1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetingsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmpTab",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmpRole = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmpEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmpPassword = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Available = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmpTab", x => x.EmpId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomTab",
+                columns: table => new
+                {
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    Available = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomTab", x => x.RoomId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SlotTab",
+                columns: table => new
+                {
+                    SlotId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    EmpId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    STime = table.Column<TimeOnly>(type: "time", nullable: false),
+                    ETime = table.Column<TimeOnly>(type: "time", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SlotTab", x => x.SlotId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmpTab");
+
+            migrationBuilder.DropTable(
+                name: "RoomTab");
+
+            migrationBuilder.DropTable(
+                name: "SlotTab");
+        }
+    }
+}
