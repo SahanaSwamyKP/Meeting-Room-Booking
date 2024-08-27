d8be95172e7d2e0bf0a1dee9ec0359f51818b923
Prajwal <105980527+Prajwalbs123@users.noreply.github.com>
2024-08-27 20:58:00 +0530

    Add files via upload

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using MeetingsAPI.DTO;
+using MeetingsAPI.Services;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CoreWebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[EnableCors("cors")]
+	public class AuthController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+		private readonly IEmployeeServices employeeServices;
+		private const string privateKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		public AuthController(IConfiguration configuration, IEmployeeServices Employeeservices)
+		{
+			_configuration = configuration;
+			this.employeeServices = Employeeservices;
+		}
+
+		[HttpPost("login")]
+		public IActionResult Login(EmpDto empDto)
+		{
+			{
+				// Validate the user credentials (this is just a dummy check, replace with your logic)
+				try
+				{
+					EmpDto? emp = this.employeeServices.GetEmailForChange(empDto);
+					if (emp != null)
+					{
+						var token = GenerateJwtToken(emp.EmpName);
+						return Ok(new { token });
+					}
+					else
+					{
+						return Unauthorized();
+					}
+				}
+				catch (Exception ex)
+				{
+					return new JsonResult(ex.Message);
+				}
+
+			}
+
+		}
+		private string GenerateJwtToken(string username)
+		{
+			var key = Encoding.UTF8.GetBytes(privateKey);
+			var tokenDesc = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(new[]
+			{
+				new Claim(ClaimTypes.Name, username)
+			}),
+				Expires = DateTime.UtcNow.AddHours(1),
+				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+			};
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var token = tokenHandler.CreateToken(tokenDesc);
+			return tokenHandler.WriteToken(token);
+		}
+	}
+}
---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,119 @@
+﻿using MeetingsAPI.DTO;
+using MeetingsAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingsAPI.Controllers
+{
+	//[Authorize]
+	[Route("api/[controller]")]
+	[ApiController]
+	[EnableCors]
+	public class EmployeesController : ControllerBase
+	{
+		private readonly IEmployeeServices employeeServices;
+		public EmployeesController(IEmployeeServices employeeServices)
+		{
+			this.employeeServices = employeeServices;
+		}
+
+		[HttpGet]
+		public IActionResult GetEmployees()
+		{
+			return new JsonResult(employeeServices.GetEmployees());
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetEmpById(int id)
+		{
+			try
+			{
+				var empDto = new EmpDto { EmpId = id };
+				var result = employeeServices.GetEmpbyId(empDto);
+				return new JsonResult(result);
+			}
+			catch (Exception ex)
+			{
+				return new JsonResult(new { message = ex.Message });
+			}
+		}
+
+		//[Authorize]
+		[HttpPost]
+		[Route("Email")]
+		public IActionResult GetEmp(EmpDto empDto)
+		{
+			EmpDto emp = empDto;
+			try
+			{
+
+				emp = employeeServices.GetEmpbyEmail(empDto);
+				if (emp == null) throw new Exception("Email not found! Contact Admin");
+			}
+			catch (Exception ex)
+			{
+				return new JsonResult(ex.Message);
+			}
+			return new JsonResult(emp);
+		}
+
+		[HttpPost]
+		public IActionResult AddEmployee(EmpDto empDto)
+		{
+			try
+			{
+				employeeServices.AddEmployee(empDto);
+			}
+			catch (Exception ex)
+			{
+				return new JsonResult(ex.Message);
+			}
+
+			return new JsonResult("Employee Added");
+		}
+
+		[HttpPut]
+		[Route("ChangePassword")]
+		public IActionResult UpdatePassword(EmpDto empDto)
+		{
+			try
+			{
+				employeeServices.UpdatePassword(empDto);
+			}
+			catch (Exception ex)
+			{
+				return new JsonResult(ex.Message);
+			}
+			return new JsonResult("Password Updated Successfully");
+		}
+
+		[HttpPut]
+		public IActionResult UpdateEmployee(EmpDto empDto)
+		{
+			try
+			{
+				employeeServices.UpdateEmployee(empDto);
+			}
+			catch (Exception ex)
+			{
+				return new JsonResult(ex.Message);
+			}
+			return new JsonResult("Updated Changes");
+		}
+
+		[HttpPut("{id}")]
+		public ActionResult UpdateEmployeeId(int id, [FromBody] EmpDto empDto)
+		{
+			try
+			{
+				employeeServices.UpdateEmployeeById(id, empDto);
+
+			}
+			catch (Exception ex)
+			{
+				return new JsonResult(ex.Message);
+			}
+			return new JsonResult("Updated");
+		}
+	}
+}
